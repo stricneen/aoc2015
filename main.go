29750d8399704d65440399fd708144ec3efd4770
Paul Brown <paul.brown@bjss.com>
2020-05-16 15:54:03 +0100

    Day 1

--- main.go
+++ main.go
@@ -1,23 +1,44 @@
 package main
 
 import (
-	//   "bufio"
-	"fmt"
 	//   "io"
+	"fmt"
 	"io/ioutil"
-	//   "os"
+	"strings"
 )
+
+func main() {
+	data := input()
+
+	// part 1
+	up := strings.Count(data, "(")
+	down := strings.Count(data, ")")
+	fmt.Println(up - down)
+
+	// part 2
+	c := 0
+	for i, rune := range data {
+		if c < 0 {
+			fmt.Printf("%d\n", i)
+			break
+		}
+		if rune == '(' {
+			c += 1
+		}
+		if rune == ')' {
+			c -= 1
+		}
+	}
+}
+
+func input() string {
+	dat, err := ioutil.ReadFile("day1.txt")
+	check(err)
+	return string(dat)
+}
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
-}
-
-func main() {
-
-	dat, err := ioutil.ReadFile("day1.txt")
-	check(err)
-	fmt.Print(string(dat))
-
 }

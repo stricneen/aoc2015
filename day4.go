47bc9aa0e3d5cf20377390bf60735ebccca7e7ec
Paul Brown <paul.brown@bjss.com>
2020-05-17 12:28:48 +0100

    Day 4-1

---
+++ day4.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+)
+
+// Day4 is here
+func Day4() {
+
+	input := "bgvyzdsv"
+	c := 1
+
+	for c > 0 {
+		data := input + strconv.Itoa(c)
+		hash := md5Hash(data)
+		if hash[0:5] == "00000" {
+			break
+		}
+		c++
+	}
+	fmt.Println(c)
+}
+
+func md5Hash(text string) string {
+	algorithm := md5.New()
+	algorithm.Write([]byte(text))
+	return hex.EncodeToString(algorithm.Sum(nil))
+}
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 //   "io"
 
 func main() {
-	Day3()
+	Day4()
 }

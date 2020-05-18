a5cb5c64b925c5bfab6e5493f9b7ea60c0fc8607
Paul Brown <paul.brown@bjss.com>
2020-05-18 12:56:31 +0100

    Day 6-1 solved

--- day6.go
+++ day6.go
@@ -7,20 +7,57 @@ import (
 	"strings"
 )
 
+type rect struct {
+	x1 int
+	y1 int
+	x2 int
+	y2 int
+}
+
 func onScan6(scan *bufio.Scanner) {
 
 	var lights [1000][1000]bool
 
 	for scan.Scan() {
 		ln := scan.Text()
-		cmd, x1, y1, x2, y2 := parse(ln)
+		cmd, r := parse(ln)
 
-		fmt.Println(ln)
-		fmt.Println(cmd, x1, y1, x2, y2)
+		switch cmd {
+		case "toggle":
+			toggle(&lights, r)
+		case "on":
+			on(&lights, r)
+		case "off":
+			off(&lights, r)
+		}
 	}
 
 	howMany(lights)
 
+}
+
+func toggle(mat *[1000][1000]bool, r rect) {
+	for i := r.x1; i <= r.x2; i++ {
+		for j := r.y1; j <= r.y2; j++ {
+			mat[i][j] = !mat[i][j]
+		}
+	}
+}
+
+func on(mat *[1000][1000]bool, r rect) {
+	for i := r.x1; i <= r.x2; i++ {
+		for j := r.y1; j <= r.y2; j++ {
+			mat[i][j] = true
+		}
+	}
+}
+
+func off(mat *[1000][1000]bool, r rect) {
+	for i := r.x1; i <= r.x2; i++ {
+		for j := r.y1; j <= r.y2; j++ {
+			mat[i][j] = false
+		}
+	}
 }
 
 func parseCoords(coords string) (int, int) {
@@ -32,17 +69,17 @@ func parseCoords(coords string) (int, int) {
 	return x, y
 }
 
-func parse(ln string) (string, int, int, int, int) {
+func parse(ln string) (string, rect) {
 	s := strings.Split(ln, " ")
 
 	if s[0] == "toggle" {
 		x1, y1 := parseCoords(s[1])
 		x2, y2 := parseCoords(s[3])
-		return "toggle", x1, y1, x2, y2
+		return "toggle", rect{x1, y1, x2, y2}
 	} else {
 		x1, y1 := parseCoords(s[2])
 		x2, y2 := parseCoords(s[4])
-		return s[1], x1, y1, x2, y2
+		return s[1], rect{x1, y1, x2, y2}
 	}
 
 }

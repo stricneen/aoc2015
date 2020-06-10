2940edaf55c2b63010daf33878e16d1f03f01c4c
Paul Brown <paul.brown@bjss.com>
2020-06-10 16:26:13 +0100

    Day 24 setup

--- day24.go
+++ day24.go
@@ -3,17 +3,33 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"strconv"
 )
+
+func exec24(p []int) {
+	t := 0
+	for _, v := range p {
+		t += v
+	}
+
+	fmt.Println("Total", t, "Group", t/3)
+
+}
 
 func onScan24(scan *bufio.Scanner) {
 
+	p := make([]int, 0)
 	for scan.Scan() {
 		ln := scan.Text()
+		t, _ := strconv.Atoi(ln)
+		p = append(p, t)
+
 		if ln == "#" {
 			break
 		}
-		fmt.Println(ln)
+		//fmt.Println(ln)
 	}
+	exec24(p)
 
 }
 

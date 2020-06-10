e3e333e25c90489b20493e93ddaea531cc492698
Paul Brown <paul.brown@bjss.com>
2020-06-10 13:25:01 +0100

    Still 1242

--- day22.go
+++ day22.go
@@ -22,6 +22,14 @@ type game struct {
 	manaSpent int
 }
 
+const missile = "missile"
+const shield = "shield"
+const drain = "drain"
+const poison = "poison"
+const recharge = "recharge"
+const wizard = "wizard"
+const boss = "boss"
+
 // Day22 is
 func Day22() {
 
@@ -33,7 +41,7 @@ func Day22() {
 
 		next := make([]game, 0)
 		for _, v := range results {
-			if v.winner == "wizard" || v.winner == "" {
+			if v.winner == wizard || v.winner == "" {
 				next = append(next, v)
 			}
 		}
@@ -42,7 +50,7 @@ func Day22() {
 
 	minMana := 100000
 	for _, v := range results {
-		if v.winner == "wizard" && v.manaSpent < minMana {
+		if v.winner == wizard && v.manaSpent < minMana {
 			minMana = v.manaSpent
 		}
 	}
@@ -52,25 +60,19 @@ func Day22() {
 func initialGameStateTest1() game {
 	effects := make([]effect, 0)
 	return game{50, 500, 51, 9, effects, "", 0} // 900
-	//return game{10, 250, 14, 8, effects, "", 0} // 641
-	//return game{10, 250, 13, 8, effects, "", 0} // 226
+	return game{10, 250, 14, 8, effects, "", 0} // 641
+	return game{10, 250, 13, 8, effects, "", 0} // 226
 }
 
 func turn(c game, spell effectFn) game {
+	g := c
+	//g := clone(c)
+	g.playerHitPoints--
 
-	g := game{
-		c.playerHitPoints - 1,
-		c.playerMama,
-		c.bossHitPoints,
-		c.bossDamage,
-		c.effects,
-		c.winner,
-		c.manaSpent}
-
-	if g.playerHitPoints == 0 {
-		g.winner = "boss"
-		return g
-	}
+	// if g.playerHitPoints < 1 {
+	// 	g.winner = boss
+	// 	return g
+	// }
 
 	// Apply effects
 	g = applyEffects(g)
@@ -95,18 +97,18 @@ func turn(c game, spell effectFn) game {
 
 }
 
-func hard(g game) game {
-	r := g
-	r.playerHitPoints--
-	return checkWin(r)
-}
+// func hard(g game) game {
+// 	r := g
+// 	r.playerHitPoints--
+// 	return checkWin(r)
+// }
 
 // Part 1 : 900
 // Part 2 : 1242 th ?
 
 func armor(g game) int {
 	for _, v := range g.effects {
-		if v.name == "shield" {
+		if v.name == shield {
 			return 7
 		}
 	}
@@ -115,26 +117,27 @@ func armor(g game) int {
 
 func applyEffects(g game) game {
 	n := g
+
 	n.effects = make([]effect, 0)
 
 	for _, spell := range g.effects {
-		if spell.name == "poison" {
+		if spell.name == poison {
 			n.bossHitPoints -= 3
 			if spell.ttl > 1 {
-				n.effects = append(n.effects, effect{"poison", spell.ttl - 1})
+				n.effects = append(n.effects, effect{poison, spell.ttl - 1})
 			}
 		}
 
-		if spell.name == "recharge" {
+		if spell.name == recharge {
 			n.playerMama += 101
 			if spell.ttl > 1 {
-				n.effects = append(n.effects, effect{"recharge", spell.ttl - 1})
+				n.effects = append(n.effects, effect{recharge, spell.ttl - 1})
 			}
 		}
 
-		if spell.name == "shield" {
+		if spell.name == shield {
 			if spell.ttl > 1 {
-				n.effects = append(n.effects, effect{"shield", spell.ttl - 1})
+				n.effects = append(n.effects, effect{shield, spell.ttl - 1})
 			}
 		}
 	}
@@ -142,9 +145,14 @@ func applyEffects(g game) game {
 	return checkWin(n)
 }
 
-// th 1242
+func clone(g game) game {
+	tmp := make([]effect, len(g.effects))
+	copy(g.effects, tmp)
+	n := game{g.playerHitPoints, g.playerMama, g.bossHitPoints, g.bossDamage, tmp, g.winner, g.manaSpent}
+	return n
+}
 
-var min int = 10000
+// th 1242
 
 func tick(games []game) []game {
 
@@ -157,35 +165,35 @@ func tick(games []game) []game {
 			continue
 		}
 
-		if canCast(g, "missle", 53) {
+		if canCast(g, missile, 53) {
 			next = append(next, turn(g, func(g game) game {
 				return game{g.playerHitPoints, g.playerMama - 53, g.bossHitPoints - 4, g.bossDamage, g.effects, "", g.manaSpent + 53}
 			}))
 		}
 
-		if canCast(g, "recharge", 229) {
+		if canCast(g, recharge, 229) {
 			next = append(next, turn(g, func(g game) game {
-				g.effects = append(g.effects, effect{"recharge", 5})
+				g.effects = append(g.effects, effect{recharge, 5})
 				return game{g.playerHitPoints, g.playerMama - 229, g.bossHitPoints, g.bossDamage, g.effects, "", g.manaSpent + 229}
 			}))
 		}
 
-		if canCast(g, "shield", 113) {
+		if canCast(g, shield, 113) {
 			next = append(next, turn(g, func(g game) game {
-				g.effects = append(g.effects, effect{"shield", 6})
+				g.effects = append(g.effects, effect{shield, 6})
 				return game{g.playerHitPoints, g.playerMama - 113, g.bossHitPoints, g.bossDamage, g.effects, "", g.manaSpent + 113}
 			}))
 		}
 
-		if canCast(g, "drain", 73) {
+		if canCast(g, drain, 73) {
 			next = append(next, turn(g, func(g game) game {
 				return game{g.playerHitPoints + 2, g.playerMama - 73, g.bossHitPoints - 2, g.bossDamage, g.effects, "", g.manaSpent + 73}
 			}))
 		}
 
-		if canCast(g, "poison", 173) {
+		if canCast(g, poison, 173) {
 			next = append(next, turn(g, func(g game) game {
-				g.effects = append(g.effects, effect{"poison", 6})
+				g.effects = append(g.effects, effect{poison, 6})
 				return game{g.playerHitPoints, g.playerMama - 173, g.bossHitPoints, g.bossDamage, g.effects, "", g.manaSpent + 173}
 			}))
 		}
@@ -222,16 +230,12 @@ func allWon(games []game) bool {
 func checkWin(g game) game {
 	r := g
 
-	if g.playerHitPoints < 1 && g.bossHitPoints < 1 {
-		fmt.Println("fff")
-	}
-
 	if g.playerHitPoints < 1 {
-		r.winner = "boss"
+		r.winner = boss
 		//fmt.Println(r)
 	}
 	if g.bossHitPoints < 1 {
-		r.winner = "wizard"
+		r.winner = wizard
 		//fmt.Println(r)
 	}
 	return r

43957e5b82bbb3ae1e604ee8dcd9e790022394d6
Paul Brown <paul.brown@bjss.com>
2020-06-10 16:18:29 +0100

    Day 24 setup

--- day24.go
+++ day24.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+)
+
+func onScan24(scan *bufio.Scanner) {
+
+	for scan.Scan() {
+		ln := scan.Text()
+		if ln == "#" {
+			break
+		}
+		fmt.Println(ln)
+	}
+
+}
+
+// Day24 is here
+func Day24() {
+	scan("data/day24.txt", onScan24)
+}
--- main.go
+++ main.go
@@ -2,6 +2,6 @@ package main
 
 func main() {
 
-	Day23()
+	Day24()
 
 }

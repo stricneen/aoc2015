d700cdcfa08d02cea6f56885b52fdf77a9828f8a
Paul Brown <paul.brown@bjss.com>
2020-06-14 17:55:06 +0100

    Day 25 complete!!"

--- day25.go
+++ day25.go
@@ -14,7 +14,29 @@ func day25() {
 	fmt.Println(number(4, 2))
 	fmt.Println(number(1, 5))
 
+	pos := number(2981, 3075)
+	fmt.Println(pos)
+
+	fmt.Println(countTo(pos))
+
+	for i := 1; i < 10; i++ {
+		fmt.Println(i, countTo(i))
+	}
 }
+
+func countTo(to int) int {
+
+	acc := 20151125
+
+	for i := 1; i < to; i++ {
+		acc *= 252533
+		acc = acc % 33554393
+	}
+
+	return acc
+}
+
+// th 28836990
 
 func number(r int, c int) int {
 
